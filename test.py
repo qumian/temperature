237451c1bd536aa4118ae3e195f62f617a54a7c8
有饭 <72686039+youfan169@users.noreply.github.com>
2021-09-13 10:43:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import os
+
+name = os.environ["NAME"]
+account = os.environ["ACCOUNT"]
+password = os.environ["PASSWORD"]
+qq = os.environ["QQ"]
 print("测试的项目1")
 print("测试的项目2")
-SCKEY = os.environ["NAME"]
-print(SCKEY)
+print(name)
+print(account)
+print(password)
+print(qq)
